651fcc25a21c094b31002071c8084344f1c5a036
siddhart1435 <siddharth.shankar@in.ibm.com>
2025-02-13 12:04:34 +0530

    Add project files.

---
+++ Data/ApplicationDBContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PracticeAzureWebApp.Models;
+
+namespace PracticeAzureWebApp.Data
+{
+    public class ApplicationDBContext:DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options):base(options) { }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PracticeAzureWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
